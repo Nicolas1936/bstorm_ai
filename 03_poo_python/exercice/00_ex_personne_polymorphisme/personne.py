608fd3ec88fd575e7480fa597af00be77ffbb542
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2021-05-01 00:47:47 +0200

    ajout des differentes class

---
+++ 03_poo_python/exercice/00_ex_personne_polymorphisme/personne.py
@@ -0,0 +1,9 @@
+class Personne:
+    #INITIALISATION
+    def __init__(self, nom, prenom, date_naissance):
+        self._nom = nom
+        self._prenom = prenom
+        self._date_naissance = date_naissance
+
+    def affiche(self):
+        pass
