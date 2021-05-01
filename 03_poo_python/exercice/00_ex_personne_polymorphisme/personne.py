bd9099c70cda9d5dede4ceefc220d58d799c0507
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2021-05-01 13:15:52 +0200

    implémentation de la méthode affiche de la class Personne

--- 03_poo_python/exercice/00_ex_personne_polymorphisme/personne.py
+++ 03_poo_python/exercice/00_ex_personne_polymorphisme/personne.py
@@ -6,4 +6,11 @@ class Personne:
         self._date_naissance = date_naissance
 
     def affiche(self):
-        pass
+        print("Nom                  : ", self._nom)
+        print("Prenom               : ", self._prenom)
+        print("Date de naissance    : ", self._date_naissance)
+
+
+if __name__ == '__main__':
+    p = Personne("Blanchard", "Nicolas", "22/06/1983")
+    p.affiche()
